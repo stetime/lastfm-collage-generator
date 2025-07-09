e14151910ba9f77bba222ab2e88479db2d174b4f
Stephen Allen <720493+stetime@users.noreply.github.com>
2025-07-09 20:00:12 +0100

    cleanup: exit if critical environment variables are missing.

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,41 @@
-import express from 'express'
-import path from 'path'
-import logger from './utils/logger'
-import getUser from './lastfm'
-import getCollage from './collage'
-import helmet from 'helmet'
-import limiter from './middleware/rateLimit'
-import unknownEndpoint from './middleware/unknownEndpoint'
-import TTLCache from '@isaacs/ttlcache'
-import compression from 'compression'
-import PinoHttp from 'pino-http'
+import express from "express"
+import path from "path"
+import logger from "./utils/logger"
+import getUser from "./lastfm"
+import getCollage from "./collage"
+import helmet from "helmet"
+import limiter from "./middleware/rateLimit"
+import unknownEndpoint from "./middleware/unknownEndpoint"
+import TTLCache from "@isaacs/ttlcache"
+import compression from "compression"
+import PinoHttp from "pino-http"
+
+if (!process.env.LFMKEY) {
+  logger.error("Last.fm API key not set in environment variables")
+  process.exit(1)
+}
+
+if (!process.env.BASEURL) {
+  logger.error("Last.fm API base URL not set in environment variables")
+  process.exit(1)
+}
 
 const cache = new TTLCache<String, User>({ ttl: 1000 * 60 * 60 * 24 })
 
 const app = express()
 const port = process.env.PORT
 app.use(compression())
-app.use(express.static(path.join(__dirname, '../src/public')))
+app.use(express.static(path.join(__dirname, "../src/public")))
 app.use(helmet())
 // app.use(morgan)
 app.use(limiter)
 app.use(PinoHttp())
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/public/home.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../src/public/home.html"))
 })
 
-app.get('/api/:username/:duration', async (req, res) => {
+app.get("/api/:username/:duration", async (req, res) => {
   const { username, duration } = req.params
   const cacheKey = `${username}:${duration}`
   const cachedUser = cache.get(cacheKey)
@@ -43,8 +53,8 @@ app.get('/api/:username/:duration', async (req, res) => {
   res.json(user)
 })
 
-if (process.env.NODE_ENV === 'development') {
-  app.get('/image/:username/:duration', async (req, res) => {
+if (process.env.NODE_ENV === "development") {
+  app.get("/image/:username/:duration", async (req, res) => {
     const { username, duration } = req.params
     const user = await getUser(username, duration)
     if (!user || user.albums.length === 0) {
@@ -54,8 +64,8 @@ if (process.env.NODE_ENV === 'development') {
     }
     const base64 = await getCollage(user)
     user.b64 = `data:image/png;base64,${base64}`
-    const buffer = Buffer.from(user.b64.split(',')[1], 'base64')
-    res.type('jpeg')
+    const buffer = Buffer.from(user.b64.split(",")[1], "base64")
+    res.type("jpeg")
     res.send(buffer)
   })
 }
